8ebb31d92046286f3ce1df31610d2c1e99478706
Java4lifer <104568501+Java4lifer@users.noreply.github.com>
2023-09-05 13:00:45 +0000

    Did stuff

--- client/src/templates/App.tsx
+++ client/src/templates/App.tsx
@@ -1,40 +1,49 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import "../App.css"
 
 export default function Fun() {
     const[input, setInput] = useState("");
+    const [task, newTask] = useState<string[]>([])
     const[editask, setTasks] = useState({
         enabled: false,
         task: ''
     });
-    const [task, newTask] = useState([
-    'die',
-    'take out the trash(me)'
-])
+
+const [test, setTest] = useState(false);
+useEffect(() => {
+    const savedTask = localStorage.getItem("@cursoreact")
+    console.log(savedTask)
+}, [])
 
 function regigi() {
     if(!input){
-        alert("Put task, you mongolian")
+        alert("Put task, you mongolian.")
         return;
     }
     if(editask.enabled) {
         editaskSave();
-        setTasks({
-            enabled: false,
-            task: ""
-        })
         return;
     }
     newTask(task => [...task, input])
+    setInput("")
+    localStorage.setItem("@cursoreact", JSON.stringify([...task, input]))
 }
 function editaskSave() {
     const getIndex = task.findIndex(task => task === editask.task)
     const allTasks =  [...task];
     allTasks[getIndex] = input;
     newTask(allTasks)
+    setTasks({
+        enabled: false,
+        task: ""
+    })
+    setInput("")
+    localStorage.setItem("@cursoreact", JSON.stringify(allTasks))
 }
 function dieTask(item:string){
     const kill = task.filter(task => task !== item)
     newTask(kill)
+    localStorage.setItem("@cursoreact", JSON.stringify(kill))
 }
 function susTask(item:string) {
     setInput(item)
@@ -46,13 +55,14 @@ function susTask(item:string) {
 
  return (
         <div>
+        <button onClick={() => setTest(true)}>Let's click</button>
         <h1>!Task list!</h1>
         <input
         placeholder='Task digit'
         value={input}
         onChange={(e) => setInput(e.target.value)}
         />
-        <button onClick={regigi}> Add cringe Task</button>
+        <button onClick={regigi}>{editask.enabled ? "Fix the Task" : "Add Cringe Task"}</button>
         <hr/>
         {task.map( (item, index) => (
             <section key={item}> 
